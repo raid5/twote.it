893511c58668cfcf67774df3c747c594eca6e923
Adam McDonald <amcdonald_3@yahoo.com>
2010-06-25 21:36:17 -0700

    new rails project for twote.it

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = 'a254934dc553feb77375aeb0fd7d000f2f0373d7e0f8593f5642bfcae912cddd0df8b2c28e12cb8108675856f7799dedb95a9a8da0a7c46d0f18103a6e19020e'
