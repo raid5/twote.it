f2b181c72bd0d2f5327b7991909f7b40098ffd42
Adam McDonald <amcdonald_3@yahoo.com>
2010-07-13 21:26:03 -0700

    limiting quote input via js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,6 +2,37 @@ document.observe("dom:loaded", function() {
 
   $('submit_image').observe("click", show_submitter);
   
+  
+  new Form.Element.Observer('quote_quote', 0.2, function(el, value) {
+    var len = $('quote_quote').value.length + $('quote_author').value.length;
+    
+    if (len > 140) {
+      var diff = len - 140;
+      var quote = $('quote_quote').value;
+      $('quote_quote').value = quote.substr(0, quote.length - diff);
+    }
+    
+    
+    len = $('quote_quote').value.length + $('quote_author').value.length;
+    
+    $('quote-count').update(140 - len);
+  });
+  
+  new Form.Element.Observer('quote_author', 0.2, function(el, value) {
+    var len = $('quote_quote').value.length + $('quote_author').value.length;
+    
+    if (len > 140) {
+      var diff = len - 140;
+      var author = $('quote_author').value;
+      $('quote_author').value = author.substr(0, author.length - diff);
+    }
+    
+    
+    len = $('quote_quote').value.length + $('quote_author').value.length;
+    
+    $('quote-count').update(140 - len);
+  });
+  
 });
 
 function show_submitter() {
